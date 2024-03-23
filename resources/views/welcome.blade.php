9887932bb376e16c13bc64a966c57450161fc358
Rusty <proga100@gmail.com>
2024-03-23 17:07:43 +0500

    form gpt

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,6 +31,7 @@
                 </div>
             </div>
         @endforeach
+            
         </div>
 
         <form class="p-4 flex space-x-4 justify-center items-center" action="/" method="post">
@@ -39,5 +40,7 @@
             <input id="message" type="text" name="message" autocomplete="off" class="border rounded-md  p-2 flex-1" />
             <a class="bg-gray-800 text-white p-2 rounded-md" href="/reset">Reset Conversation</a>
         </form>
+
+
     </body>
 </html>
